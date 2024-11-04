def22ce5f7470f68c1c80ad76bb872a479f63231
sik2 <bickwang123@gmail.com>
2024-11-04 21:34:16 +0900

    검색 스타일 작업

--- src/components/SearBox.jsx
+++ src/components/SearBox.jsx
@@ -1,9 +1,14 @@
 function SearBox({ keyword, onChangeKeyword }) {
     return (
-        <div className="text-black">
-            <div>
-                <div>🔍</div>
-                <input type="text" value={keyword} onChange={onChangeKeyword} />
+        <div className="w-full mt-2">
+            <div className="relative flex items-center gap-2 bg-white rounded-full overflow-hidden shadow-md max-w-[60%] mx-auto">
+                <div className="absolute left-2 top-1/2 transform -translate-y-1/2 flex items-center">🔍</div>
+                <input
+                    className="w-full block py-2 px-5 pl-10 border-0 outline-none"
+                    placeholder="영화 이름을 입력하세요."
+                    value={keyword}
+                    onChange={onChangeKeyword}
+                />
             </div>
         </div>
     )
